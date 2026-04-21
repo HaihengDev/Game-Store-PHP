5a0d449a363e296ce1067e5a5d6841f2bc9ed4f3
HaihengDev <haihenglim9@gmail.com>
2026-04-21 18:57:39 +0700

    feat: refactor registration form and remove old login page

--- auth/register.php
+++ auth/register.php
@@ -1,21 +1,20 @@
-<?php 
-  include '../config/auth.php';
+<?php
+include '../config/auth.php';
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    if(str_contains($email, "@gmail.com")) {
-      $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?);");
-      $stmt->bind_param("ss", $username, $email, $password);
+  if (str_contains($email, "@gmail.com")) {
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?);");
+    $stmt->bind_param("ss", $username, $email, $password);
 
-      if($stmt->execute()) {
-        echo "User is registered successfully!";
-      } else {
-        echo "Error: " . $stmt->error;
-      }
-      $stmt->close();
+    if ($stmt->execute()) {
+      echo "User is registered successfully!";
+    } else {
+      echo "Error: " . $stmt->error;
     }
+    $stmt->close();
   }
-?>
+}
