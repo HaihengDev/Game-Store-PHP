06c832f578246cb3c4e0893f6a5802b8977a62a2
HaihengDev <haihenglim9@gmail.com>
2026-04-21 14:37:01 +0700

    feat: add login form to the pages directory

---
+++ pages/login.php
@@ -0,0 +1,15 @@
+<form mehtod="POST">
+  <label for="username">Username</label>
+  <input type="text" name="username" id="username" placeholder="eg. John Doe" required>
+  <label for="email">Email</label>
+  <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" required>
+  <label for="password">Password</label>
+  <input type="password" name="password" id="password" placeholder="********" required>
+  <label for="confirm">Confirm Password</label>
+  <input type="password" name="confirm" id="confirm" placeholder="********" required>
+
+  <div class="btn-register-wrapper">
+    <button type="reset">Reset</button>
+    <button type="submit">Register</button>
+  </div>
+</form>
