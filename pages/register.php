48dd430eff9d98d8bb9ec408133e63e86b0e0247
HaihengDev <haihenglim9@gmail.com>
2026-04-21 21:49:49 +0700

    feat: fix section structure in registration form for better styling

--- pages/register.php
+++ pages/register.php
@@ -1,19 +1,15 @@
-<?php
-include '../components/header.php';
-?>
-
 <form mehtod="POST">
-  <section>
+  <section class="username-wrapper">
     <label for="username">Username</label>
     <input type="text" name="username" id="username" placeholder="eg. John Doe" required>
   </section>
 
-  <section>
+  <section class="email-wrapper">
     <label for="email">Email</label>
     <input type="email" name="email" id="email" placeholder="johndoe@gmail.com" required>
   </section>
 
-  <section>
+  <section class="password-wrapper">
     <label for="password">Password</label>
     <div>
       <input type="password" name="password" id="password" placeholder="********" required>
@@ -21,7 +17,7 @@ include '../components/header.php';
     </div>
   </section>
 
-  <section>
+  <section class="confirm-wrapper">
     <label for="confirm">Confirm Password</label>
     <div>
       <input type="password" name="confirm" id="confirm" placeholder="********" required>
